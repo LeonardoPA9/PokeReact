a3c0ae1549155496cb36ab5b59cd0f1a90a25597
Emmanuel Vega <carlito_sky@hotmail.com>
2021-09-15 01:11:04 -0500

    URL matched pagination - useHistory only

--- src/components/pokelist/Pokelist.js
+++ src/components/pokelist/Pokelist.js
@@ -1,6 +1,7 @@
 import pokeEndpoint from "api/api";
 import Button from "components/UI/Button";
 import { useEffect, useReducer, useState } from "react";
+import { useHistory } from "react-router";
 import { Fragment } from "react/cjs/react.production.min";
 
 const paginationState = {
@@ -12,13 +13,13 @@ function paginationReducer(state, action) {
   switch (action.type) {
     case "INCREASE":
       return { ...state, offset: state.limit + state.offset };
-    case "DECREASE":
-      return { ...state, offset: state.offset - state.limit };
-    default:
-      break;
-  }
-}
-
+      case "DECREASE":
+        return { ...state, offset: state.offset - state.limit };
+        default:
+          break;
+        }
+      }
+      
 function Pokelist(props) {
   const [pokemonPagination, dispatchPagination] = useReducer(
     paginationReducer,
@@ -26,10 +27,13 @@ function Pokelist(props) {
   );
   const [pokemonList, setPokemonList] = useState([]);
   const { offset } = pokemonPagination;
+  const history = useHistory();
+
 
   useEffect(() => {
     const doPagination = async () => {
       const res = await pokeEndpoint.pokemonPagination(pokemonPagination);
+      history.push(`/pagination?limit=${pokemonPagination.limit}&offset=${pokemonPagination.offset}`)
       setPokemonList(res.data.results);
     };
     doPagination();
